bc9dfef7a0dfa862fd151b337ea7f7006b0720ad
Francesco Pham <francesco.pham1@h-partners.com>
2024-11-12 12:09:28 +0100

    Add lxc configs, script, and systemd service to start oniro on UT
    Add lxc configs, script, and systemd service to start oniro on Ubuntu Touch on the Volla x23 phone
    
    Signed-off-by: Francesco Pham <francesco.pham@huawei.com>

---
+++ x23/utils/start-ohos.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Function to check if lightdm service is active
+check_lightdm() {
+    systemctl is-active --quiet lightdm
+}
+
+# Wait until lightdm service is running
+until check_lightdm; do
+    echo "Waiting for lightdm to start..."
+    sleep 1
+done
+
+# Lightdm is running, wait for 3 seconds
+echo "Lightdm is active. Waiting for 3 seconds..."
+sleep 3
+
+# Stop lightdm
+echo "Stopping lightdm..."
+systemctl stop lightdm
+
+# Set property to stop vendor.hwcomposer-2-3
+echo "Stopping vendor.hwcomposer-2-3..."
+setprop ctl.stop vendor.hwcomposer-2-3
+
+# Start the OpenHarmony container
+echo "Starting OpenHarmony container..."
+exec /usr/bin/lxc-start -n openharmony -F
+
